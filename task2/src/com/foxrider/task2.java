3efd6525e98243dd8bd21a3e11798a8f4fbd9e19
Anton <as005409@g.bstu.by>
2020-10-20 21:45:04 +0300

    Task 2 for Basics of software code development

---
+++ task2/src/com/foxrider/task2.java
@@ -0,0 +1,27 @@
+package com.foxrider;
+
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[]args){
+        // Initialization of Scanner
+        Scanner sc = new Scanner(System.in);
+        // input a
+        System.out.println("Input a: ");
+        double a =  sc.nextDouble();
+
+        // input b
+        System.out.println("Input b: ");
+        double b = sc.nextDouble();
+
+        // input c
+        System.out.println("Input c: ");
+        double c = sc.nextDouble();
+
+        // answer
+        double z = ((b + Math.sqrt(Math.pow(b,2) + 4*a*c))/(2*a)) - Math.pow(a,3)*c + Math.pow(b,-2);
+        System.out.println(z);
+
+
+    }
+}
