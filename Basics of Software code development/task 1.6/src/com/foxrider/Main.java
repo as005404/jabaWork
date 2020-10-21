0554ed51b10a0c2b7838d1c83cc9d89216c675ee
Anton <as005409@g.bstu.by>
2020-10-21 21:21:40 +0300

    6 task of Basics of Software code development

---
+++ Basics of Software code development/task 1.6/src/com/foxrider/Main.java
@@ -0,0 +1,32 @@
+package com.foxrider;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Initialization of Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Input x and y
+        System.out.println("Input x and y: ");
+        double x = sc.nextInt();
+        double y = sc.nextInt();
+
+        //
+        if( x == 0 && y == -1)
+            System.out.println(false);
+        else if(y > 0) {
+            if (x >= -2 && x <= 2 && y <= 4)
+                System.out.println(true);
+            else
+                System.out.println(false);
+        }
+        else{
+                if (x >= -4 && x <= 4 && y >= -4)
+                    System.out.println(true);
+                else
+                    System.out.println(false);
+            }
+    }
+}
